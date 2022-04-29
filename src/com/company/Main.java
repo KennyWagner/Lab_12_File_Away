2547ab788d03e52e6c82a03b63c3ac27f8d1283a
KennyWagner <wagnerkenneth72@gmail.com>
2022-04-29 07:23:34 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,73 @@
+package com.company;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+
+        ArrayList<String> lines = new ArrayList<>();
+
+        try
+        {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                // Finally we can read the file LOL!
+                int line = 0;
+                int words = 0;
+                long characters = selectedFile.length();
+
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    String str[] = rec.split(" ");
+                    words = words + str.length;
+                    line++;
+
+                }
+                System.out.println(selectedFile.getName());
+                System.out.println("Number of lines: " + line);
+                System.out.println("Number of words: " + words);
+                System.out.println("Number of characters: " + characters);
+
+
+                reader.close(); // must close the file to seal it and flush buffer
+            }
+            else  // user closed the file dialog wihtout choosing
+            {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+        }  // end of TRY
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
